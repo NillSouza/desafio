b951064db3a7f69bf0afc85aa376c01ef569a2c0
Nill Souza <nill_maikel@hotmail.com>
2024-05-01 21:57:59 -0400

    Desafio

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("asdasd");
+let xp = 10001
+let classificador = 0;
+let NameHero = "Link"
+let classe = ["Ferro","Bronze","Prata","Ouro","Platina","Ascendente","Imortal","Radiante"]
+
+if(xp >= 1001 && xp<= 2000){
+    classificador = 1;
+} else
+if(xp >= 2001 && xp<= 5000){
+    classificador = 2;
+} else
+if(xp >= 5001 && xp<= 7000){
+    classificador = 3;
+} else
+if(xp >= 7001 && xp<= 8000){
+    classificador = 4;
+} else
+if(xp >= 8001 && xp<= 9000){
+    classificador = 5;
+} else
+if(xp >= 9001 && xp<= 10000){
+    classificador = 6;
+} else
+if(xp >= 10001){
+    classificador = 7;
+}
+
+console.log("O Herói de nome "+ NameHero, "está no nível de " + classe[classificador])
